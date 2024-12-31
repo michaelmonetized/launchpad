cd9ff46ba7605b0099e351d16830d77ae2c24e3d
Michael Monetized <michaelmonetized@gmail.com>
2024-12-31 04:13:33 -0500

    boilerplating and placeholding
    we need to deploy this on vercel super soon

---
+++ providers/posthog.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_API_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_API_HOST,
+    person_profiles: "always",
+  });
+}
+
+export default function PostHogProviderWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+}
